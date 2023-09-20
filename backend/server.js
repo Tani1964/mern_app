4605000a519ff40f44ad743adf82a8699348f52b
Tani1964 <ifegbesan6@gmail.com>
2023-09-20 23:31:51 +0100

    'frontend_redux_goals'

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ connectDB();
 //intializing express
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
