7b228f83c958ea44a9a40b1b11517f0609f0c6ce
Tani1964 <ifegbesan6@gmail.com>
2023-09-22 05:26:24 +0100

    '.'

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,3 +1,22 @@
 import axios from "axios";
 
 const API_URl  = '/api/goals/' 
+
+// Create new goal
+const createGoal = async (goalData, token) => {
+    const config = {
+        headers:{
+            Authorization: `Bearer ${token}`
+        }
+    }
+    
+    const response = await axios.post(API_URl, goalData, config)
+
+    return response.data
+}
+
+const goalService = {
+    createGoal,
+}
+
+export default goalService
