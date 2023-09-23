cee1786852a81b382ad564cfe6f8892d07227992
Tani1964 <ifegbesan6@gmail.com>
2023-09-23 13:05:09 +0100

    'Finishing_up'

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 // This is the entry point to our server
-const express = require('express');
-const colors = require('colors');
-const {errorHandler} = require('./middleware/errorMiddleware');
-const connectDB = require('./config/db');
-const dotenv = require('dotenv').config();
+const path = require("path");
+const express = require("express");
+const colors = require("colors");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv").config();
 
 connectDB();
 
@@ -13,13 +14,26 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/goals', require('./routes/goalRoutes'));
-app.use('/api/users', require('./routes/userRoutes'));
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+
+// Build frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 app.use(errorHandler);
 
-app.listen(port, ()=>{
-    console.log(`This Server is running on port: ${port}`);
+app.listen(port, () => {
+  console.log(`This Server is running on port: ${port}`);
 });
